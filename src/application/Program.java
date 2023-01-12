98b60e1498afc25b3cfcac800cd526be96d3e3ad
Felipe Schinaider <felipeschinaidernt@gmail.com>
2023-01-12 02:44:27 -0400

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Departament obj = new Departament(1, "books");
-		System.out.println(obj);
-
+		Seller seller = new Seller(21, "ann", "ann@gmail", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
-
 }
