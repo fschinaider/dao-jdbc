0d96a987dd60f3d8a7ff512b0e8bdf4788bb2125
Felipe Schinaider <felipeschinaidernt@gmail.com>
2023-01-12 02:59:48 -0400

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Departament;
 import model.entities.Seller;
 
@@ -10,6 +12,7 @@ public class Program {
 	public static void main(String[] args) {
 		Departament obj = new Departament(1, "books");
 		Seller seller = new Seller(21, "ann", "ann@gmail", new Date(), 3000.0, obj);
-		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+ 		System.out.println(seller);
 	}
 }
