fc34915d5cee435a70bb50746a70bbd8535ad2c6
Felipe Schinaider <felipeschinaidernt@gmail.com>
2023-01-12 06:50:50 -0400

    DepartmentDao

---
+++ src/application/Program2.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.List;
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+		System.out.println("teste 1: findById");
+		Department dep = departmentDao.findById(3);
+		System.out.println(dep);
+
+		System.out.println("teste 2: findAll");
+		List<Department> list = departmentDao.findAll();
+		for (Department d : list) {
+			System.out.println(d);
+		}
+
+		System.out.println("teste 3: insert");
+		Department newDepartment = new Department(null, "Design");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id: " + newDepartment.getId());
+
+		System.out.println("teste 4: update");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Shops");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+
+		System.out.println("teste 5: delete");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+
+		sc.close();
+	}
+}
